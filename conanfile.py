4f1c2d8ab2e06cac66dd6a50d4f1fdb67ecd9db7
miryo <miryo@vcup.moe>
2023-06-29 19:31:33 +0900

    deps: use conanfile.py for ninja compatible

---
+++ conanfile.py
@@ -0,0 +1,17 @@
+from conan import ConanFile
+from conan.tools.cmake import CMakeToolchain, CMakeDeps
+
+
+class TileMapSplicer(ConanFile):
+    name = "TileMapSplicer"
+    version = "0.0.0"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = "libvips/8.14.2"
+
+    def generate(self):
+        toolchain = CMakeToolchain(self)
+        toolchain.generator = "Ninja"
+        toolchain.generate()
+
+        deps = CMakeDeps(self)
+        deps.generate()
